07d5e3d412f135b3be7fdeec3ce9d39f86351781
Liron London <limon.lon@gmail.com>
2018-03-26 15:00:28 +0300

    updated parse_board.cpp

--- AT_EX1/parse_board.cpp
+++ AT_EX1/parse_board.cpp
@@ -3,7 +3,7 @@
 #include "parse_board.h"
 //using namespace std;
 
-// int checkline(std::string line) {
+// int checkLin (std::string line) {
 //   std::size_t found = line.find("<");
 //   if (found!=std::string::npos) {
 //     std::cout << "first '<' found at: " << found << std::endl;
@@ -13,48 +13,94 @@
 //   }
 // }
 
-int checkPieces
+class ParseBoard {
+  int pieceCount = {R, P, S, B, J, F};
+  int curPlayer;
+  int curLine;
 
-int checkline(std::string line) {
-  // If joker line:
-  if (line[0] == 'J') {
-    if (line.length() != 10 || line[1] != "<"  || line[2] != 'J' || line[4] != "<" || line[7] != "<" ||
-       line[3] != ">" || line[6] != ">" || line[9] != ">") {
+public:
+
+  int checkPieces() {
+    for (int i = 0; i < sizeof(pieceCount); i++) {
+      if (pieceCount[i] < 0) {
+        std::cout << "Board file format error: too many pieces of same type." << std::endl;
+        return curPlayer;
+      }
+      if (pieceCount[-1] > 0) {
+        std::cout << "Board file format error: flag not placed on board." << std::endl;
+        return curPlayer;
+      }
+    }
+    return 0;
+  }
+
+  int updatePieceCount (std::string line) {
+    switch(line[1]) {
+      case 'R': pieceCount[0]--;
+      break;
+      case 'P': pieceCount[1]--;
+      break;
+      case 'S': pieceCount[2]--;
+      break;
+      case 'B': pieceCount[3]--;
+      break;
+      case '<': pieceCount[4]--; //this is the joker
+      break;
+      case 'F': pieceCount[5]--;
+      break;
+    }
+  }
+
+  int checkLine (std::string line) {
+    // If joker line:
+    if (line[0] == 'J') {
+      if (line.length() != 10 || line[1] != "<"  || line[2] != 'J' || line[4] != "<" || line[7] != "<" ||
+        line[3] != ">" || line[6] != ">" || line[9] != ">") {
+        std::cout << "Format error in board file." << std:endl;
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+    // If not joker line:
+    if (line.length() != 9 || line[0] != "<"  || line[1] == 'J' || line[3] != "<" || line[6] != "<" ||
+        line[2] != ">" || line[5] != ">" || line[8] != ">") {
       std::cout << "Format error in board file." << std:endl;
-      return 1;
+        return 1;
     } else {
       return 0;
     }
   }
-  // If not joker line:
-  if (line.length() != 9 || line[0] != "<"  || line[1] == 'J' || line[3] != "<" || line[6] != "<" ||
-       line[2] != ">" || line[5] != ">" || line[8] != ">") {
-    std::cout << "Format error in board file." << std:endl;
-      return 1;
-  } else {
-    return 0;
-  }
-}
 
-
-// int read_from_file () {
-int main (int argc, char* argv[]) {
-  std::string line;
-  int res = -1;
-  std::ifstream myfile (argv[1]);
-  if (myfile.is_open()) {
-    while (getline (myfile,line)) {     //getline returns a reference to myfile
-      res = checkline(line);
+  // gets line, checks format, calls all aiding functions
+  int parseBoardFile (std::string filename) {
+    std::string line;
+    int res = -1;
+    std::ifstream myfile (filename);
+    if (myfile.is_open()) {
+      curPlayer = filename[6] - '0'; // gets the int value of the char
+      while (getline (myfile,line)) {     //getline returns a reference to myfile
+        curLine++;
+        res = checkLine (line);
+        if (res == 0) {
+          res = updatePieceCount (line);
+        } else {
+          return res;
+        }
+      }
+      myfile.close();
+      res = checkPieces();
     }
-    myfile.close();
+    else {
+        std::cout << "Unable to open file" << std::endl; 
+        return 1;
+    }
+    std::cout << "res is: " << res << std::endl;
+    return res;
   }
-  else {
-      std::cout << "Unable to open file" << std::endl; 
-      return 1;
-  }
-  std::cout << "res is: " << res << std::endl;
-  return 0;
-}
+};
+
+
 
 
 // int main () {
