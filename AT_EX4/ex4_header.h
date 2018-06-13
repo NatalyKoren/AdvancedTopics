324b9661ce3e8d7bf9cfe214255815fd06caf933
Nataly Koren <natalykoren@mail.tau.ac.il>
2018-06-13 14:35:13 +0300

    done EX4.

--- AT_EX4/ex4_header.h
+++ AT_EX4/ex4_header.h
@@ -1,9 +1,134 @@
 //
-// Created by DELL on 12/06/2018.
+// Created by DELL on 07/06/2018.
 //
 
-#ifndef AT_EX4_EX4_HEADER_H
-#define AT_EX4_EX4_HEADER_H
+#ifndef AT_EX4_GAMEBOARD_H
+#define AT_EX4_GAMEBOARD_H
 
-#include "GameBoard.h"
-#endif //AT_EX4_EX4_HEADER_H
+
+
+#include <memory>
+#include <functional>
+#include <tuple>
+
+template<typename GAME_PIECE>
+using PieceInfo = std::unique_ptr<const std::pair<int, GAME_PIECE>>;
+
+template <int ROWS, int COLS, typename GAME_PIECE, int PLAYERS=2>
+class GameBoard{
+    PieceInfo<GAME_PIECE> m_mainBoard[ROWS][COLS];
+
+public:
+    PieceInfo<GAME_PIECE> getPiece(int row, int col) {
+        if(row < 0 || row >= ROWS) return nullptr;
+        if(col < 0 || col >= COLS) return nullptr;
+        if(m_mainBoard[row][col] == nullptr) return nullptr;
+        return std::make_unique<const std::pair<int, GAME_PIECE>>(
+                std::make_pair(m_mainBoard[row][col]->first,m_mainBoard[row][col]->second));
+    }
+    PieceInfo<GAME_PIECE> setPiece(int row, int col, GAME_PIECE piece, int player){
+        if(row < 0 || row >= ROWS) return nullptr;
+        if(col < 0 || col >= COLS) return nullptr;
+        PieceInfo<GAME_PIECE> prevVal = std::move(m_mainBoard[row][col]);
+        m_mainBoard[row][col] = std::make_unique<const std::pair<int, GAME_PIECE>>(std::make_pair(player, piece));
+        return prevVal;
+    }
+
+
+    class PredicateIterator{
+        const GameBoard* m_board;
+        int m_rows;
+        int m_cols;
+        std::function<bool(const PieceInfo<GAME_PIECE>&)> m_predicate;
+    public:
+        PredicateIterator(const GameBoard* board, std::function<bool(const PieceInfo<GAME_PIECE>&)> predicate, int rows, int cols)
+                : m_board(board), m_rows(rows), m_cols(cols), m_predicate(predicate) {}
+
+        const std::tuple<int, int, GAME_PIECE, int> operator*(){
+            int player = m_board->m_mainBoard[m_rows][m_cols]->first;
+            GAME_PIECE pie(m_board->m_mainBoard[m_rows][m_cols]->second);
+            return std::make_tuple(m_rows,m_cols,pie,player);
+        };
+        bool operator!=(const PredicateIterator& other){
+            return (m_rows != other.m_rows) || (m_cols != other.m_cols) || (m_board != other.m_board);
+        }
+        PredicateIterator& operator++(){
+            if(m_rows >= ROWS) return *this;
+            do{
+                m_cols++;
+                if(m_cols == COLS){
+                    m_cols = 0;
+                    m_rows++;
+                    if(m_rows >= ROWS){
+                        m_cols = COLS;
+                        break;
+                    }
+                }
+
+            }while(!m_predicate(m_board->m_mainBoard[m_rows][m_cols]));
+            return *this;
+        }
+
+        bool isPredicate(int row, int col) const{return m_predicate(m_board->m_mainBoard[row][col]);}
+
+    };
+
+    PredicateIterator begin()const{
+        PredicateIterator itr(this, [](const PieceInfo<GAME_PIECE>& piece){ return (piece!= nullptr); }, 0, 0);
+        if(!itr.isPredicate(0,0))
+            ++itr;
+        return itr;
+    }
+    PredicateIterator end()const{
+        return PredicateIterator(this, [](const PieceInfo<GAME_PIECE>& piece){ return (piece!= nullptr); }, ROWS, COLS);
+    }
+
+    class GameBoardView{
+        const GameBoard* m_board;
+        std::function<bool(const PieceInfo<GAME_PIECE>&)> m_predicate;
+
+    public:
+        GameBoardView(const GameBoard* board, std::function<bool(const PieceInfo<GAME_PIECE>&)> predicate):
+                m_board(board), m_predicate(predicate) {}
+        PredicateIterator begin()const {
+            PredicateIterator itr(this->m_board,
+                                  m_predicate,
+                                  0, 0);
+            if(!itr.isPredicate(0,0))
+                ++itr;
+            return itr;
+        }
+        PredicateIterator end()const {
+            return PredicateIterator(this->m_board,
+                                     m_predicate,
+                                     ROWS, COLS);
+        }
+
+    };
+
+    GameBoardView allPiecesOfPlayer(int playerNum) const{
+        return GameBoardView(this,
+                             [playerNum](const PieceInfo<GAME_PIECE>& piece){
+                                 return (piece!= nullptr) && (piece->first == playerNum);
+                             });
+    }
+
+    GameBoardView allOccureneceOfPiece(const GAME_PIECE pieceToFind) const{
+        return GameBoardView(this,
+                          [pieceToFind](const PieceInfo<GAME_PIECE>& piece){
+                              return (piece!= nullptr) && (piece->second == pieceToFind);
+                          });
+    }
+
+    GameBoardView allOccureneceOfPieceForPlayer(const GAME_PIECE pieceToFind, int playerNum) const {
+        return GameBoardView(this,
+                             [pieceToFind, playerNum](const PieceInfo<GAME_PIECE>& piece){
+                                 return (piece!= nullptr) && (piece->second == pieceToFind) && (piece->first == playerNum);
+                             });
+    }
+
+};
+
+
+
+#endif //AT_EX4_GAMEBOARD_H
--- AT_EX4/main.cpp
+++ AT_EX4/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include "GameBoard.h"
+#include <string>
+#include <utility>
 
+#include "ex4_header.h"
+#include "unit_test_util.h"
 
-
-int main(){
+void test1(){
     GameBoard<12, 7, std::string, 4> myBoard;
     std::string piece = "nataly";
     std::string piece2 = "yotam";
@@ -48,7 +50,95 @@ int main(){
         std::cout << "player: " << std::get<3>(pieceInfo) << std::endl;
     }
 
+}
+
+static bool test2(){
+    GameBoard<12, 7, std::string, 4> board;
+    int i = 0;
+    for (int row = 0; row < 12; row++){
+        for(int col =0; col < 7; col++){
+            board.setPiece(row, col, "Piece", i);
+            i = (i+1)%4;
+        }
+    }
+    int occurence[4]= {0,0,0,0};
+    for(auto pieceInfo : board){
+        occurence[std::get<3>(pieceInfo)]++;
+    }
+    for(int i = 0;i<4;i++){
+        if (occurence[i] != 21){
+            return false;
+        }
+    }
+    return true;
+}
+
+void testYotam(){
+
+    std::cout << "hello world" << std::endl;
+    std::unique_ptr<int> uni;
+    std::cout << "is null? " << (uni== nullptr ? "true" : "false") << std::endl;
+
+    GameBoard<3, 4, std::string, 4> myBoard;
+    myBoard.setPiece(0,1,"nataly",1);
+    myBoard.setPiece(0,2,"yotam",2);
+    myBoard.setPiece(0,3,"tomer",3);
+    myBoard.setPiece(1,0,"Yotam",2);
+    myBoard.setPiece(1,1,"party",2);
+    myBoard.setPiece(1,2,"party",1);
+    myBoard.setPiece(1,3,"party",2);
+
+    std::cout << "\nSome getPiece basic checks: \n";
+    (myBoard.getPiece(0,0) ?
+     std::cout << "0,0: "<< myBoard.getPiece(0,0)->first << ", " << myBoard.getPiece(0,0)->second << std::endl :
+     std::cout << "0,0: nullptr" << std::endl);
+    (myBoard.getPiece(1,1) ?
+     std::cout << "1,1: "<< myBoard.getPiece(1,1)->first << ", " << myBoard.getPiece(1,1)->second << std::endl :
+     std::cout << "1,1: nullptr" << std::endl);
+    (myBoard.getPiece(0,1) ?
+     std::cout << "0,1: "<< myBoard.getPiece(0,1)->first << ", " << myBoard.getPiece(0,1)->second << std::endl :
+     std::cout << "0,1: nullptr" << std::endl);
+
+    std::cout << "\nIterate all pieces: \n";
+    for(auto pieceInfo : myBoard) {
+        std::cout << "row: " << std::get<0>(pieceInfo) << ", ";
+        std::cout<< "col: " << std::get<1>(pieceInfo) << ", ";
+        std::cout << "piece: <" << std::get<2>(pieceInfo) << ">, "; // we assume here that GAME_PIECE implemented <<
+        std::cout << "player: " << std::get<3>(pieceInfo) << std::endl;
+    }
+    std::cout << "\nIterate player 2 pieces: \n";
+    for(auto pieceInfo : myBoard.allPiecesOfPlayer(2)) {
+        std::cout << "row: " << std::get<0>(pieceInfo) << ", ";
+        std::cout<< "col: " << std::get<1>(pieceInfo) << ", ";
+        std::cout << "piece: <" << std::get<2>(pieceInfo) << ">, "; // we assume here that GAME_PIECE implemented <<
+        std::cout << "player: " << std::get<3>(pieceInfo) << std::endl;
+    }
+
+    std::cout << "\nIterate 'party' pieces: \n";
+    for(auto pieceInfo : myBoard.allOccureneceOfPiece("party")) {
+        std::cout << "row: " << std::get<0>(pieceInfo) << ", ";
+        std::cout<< "col: " << std::get<1>(pieceInfo) << ", ";
+        std::cout << "piece: <" << std::get<2>(pieceInfo) << ">, "; // we assume here that GAME_PIECE implemented <<
+        std::cout << "player: " << std::get<3>(pieceInfo) << std::endl;
+    }
+
+    std::cout << "\nIterate player 2 'party' pieces: \n";
+    for(auto pieceInfo : myBoard.allOccureneceOfPieceForPlayer("party", 2)) {
+        std::cout << "row: " << std::get<0>(pieceInfo) << ", ";
+        std::cout<< "col: " << std::get<1>(pieceInfo) << ", ";
+        std::cout << "piece: <" << std::get<2>(pieceInfo) << ">, "; // we assume here that GAME_PIECE implemented <<
+        std::cout << "player: " << std::get<3>(pieceInfo) << std::endl;
+    }
 
 
+    std::cout << "\ndone." << std::endl << std::flush;
+}
+
+
+int main(){
+
+    RUN_TEST(test2);
+    test1();
+    testYotam();
     return 0;
 }
